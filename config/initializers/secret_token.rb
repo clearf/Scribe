81b6c74e7649c0d544e87fd199c8a1c44ec51935
egong <egong82@gmail.com>
2013-09-02 14:17:31 -0400

    made Scribe repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scribe::Application.config.secret_token = 'd30235cbf97e07ce74cdf96b08934488834e5c23b24514c1c182d38aed5c557e36514713ee546922eb0784b3158151fc2536fc16732e23a0ea5f1c514bcd6c76'
