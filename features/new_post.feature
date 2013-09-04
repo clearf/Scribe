4c0f908147f2d9a3ea8c642b1d8328c837e97733
egong <egong82@gmail.com>
2013-09-04 13:29:10 -0400

    cucumber tests exploded, starting over

--- features/new_post.feature
+++
@@ -1,6 +0,0 @@
-Feature: New Post
-
-Scenario: Posting
-Given I'm on the index page
-When I click new post
-Then I should see the new topic page
--- features/step_definitions/index._page_steps.rb
+++ features/step_definitions/index._page_steps.rb
@@ -1,3 +1,7 @@
 Given(/^I go to the main page$/) do
   visit "/"
 end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  page.should have_content('Scribe')
+end
