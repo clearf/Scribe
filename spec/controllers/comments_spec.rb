5848fbcb252383b83ac5870a95754d42c5f143f0
egong <egong82@gmail.com>
2013-09-03 21:22:52 -0400

    heroku

---
+++ spec/controllers/comments_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../spec_helper'
+
+describe CommentsController do
+  before do
+    @comment = Comment.create(description: "The title")
+    visit comment_path(@comment)
+  end
+  describe "index" do
+    it "should have all the comments" do
+      get :index, letter: 'S'
+      expect(response).to render_template :index
+    end
+  end
+  describe "show" do
+    it "shows an individual comment" do
+      get :show, :id => 1
+       expect(response).to render_template :show
+    end
+  end
+  describe "new" do
+    it "renders new" do
+      @post = Post.create(topic: "The title", :id => 1)
+      @post.save
+      #nested route and unique controller
+      get :new, :id => 1
+       expect(response).to render_template :new
+    end
+  end
+  describe "edit" do
+    it "edits an individual comment" do
+      get :edit, :id => 1
+       expect(response).to render_template :edit
+    end
+  end
+end
